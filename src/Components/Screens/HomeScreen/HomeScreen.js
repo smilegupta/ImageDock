726cc99a5d5b1638f74efd805e0e94f2c59997fa
smilegupta <guptamiley3012@gmail.com>
2021-04-13 16:54:48 +0530

    added infographic

--- src/Components/Screens/HomeScreen/HomeScreen.js
+++ src/Components/Screens/HomeScreen/HomeScreen.js
@@ -1,46 +1,53 @@
 import React from "react";
 import { collectionList } from "../../../config/dummydata";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Image} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import NoImage from '../../../infographic.svg'
 
 const HomeScreen = () => {
-  return (<>
-    <h4 className="my-4">
-      My Collections <i className="las la-plus cursor-pointer" />
-    </h4>
-    <Row>
-      {collectionList.length > 0 ? (
-        <>
-          {" "}
-          {collectionList.map((item, idx) => (
-            <Col sm={12} lg={4} xl={3} md={6} key={idx}>
-              <Link to={`/collection/${item.collectionId}`}>
-                <div
-                  className="card rounded text-white bg-dark mb-3"
-                  style={{ maxWidth: "20rem" }}
-                >
-                  <div className="card-body text-center">
-                    <h4 className="card-title"> {item.name} </h4>
-                    <hr />
-                    <p className="card-text">{item.description}</p>
+  return (
+    <>
+      <Row>
+        {collectionList.length > 0 ? (
+          <>
+            <Col sm={12} lg={12} xl={12} md={12}>
+              <h4 className="my-4">
+                My Collections <i className="las la-plus cursor-pointer" />
+              </h4>
+            </Col>{" "}
+            {collectionList.map((item, idx) => (
+              <Col sm={12} lg={4} xl={3} md={6} key={idx}>
+                <Link to={`/collection/${item.collectionId}`}>
+                  <div
+                    className="card rounded text-white bg-dark mb-3"
+                    style={{ maxWidth: "20rem" }}
+                  >
+                    <div className="card-body text-center">
+                      <h4 className="card-title"> {item.name} </h4>
+                      <hr />
+                      <p className="card-text">{item.description}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </Col>
-          ))}{" "}
-        </>
-      ) : (
-        <Col className="text-center my-4">
-          {" "}
-          <h4> There's nothing to show here. </h4> <br />{" "}
-          <Button>
+                </Link>
+              </Col>
+            ))}{" "}
+          </>
+        ) : (
+          <Col className="text-center my-4">
             {" "}
-            &nbsp; Upload Image{" "}
-            <i className="las la-cloud-upload-alt" style={{ fontSize: "18px" }} />
-          </Button>{" "}
-        </Col>
-      )}
-    </Row>
+            <Image src={NoImage} alt="empty" height={200} />
+            <h4> There's nothing to show here. </h4> <br />{" "}
+            <Button className="btn btn-dark">
+              {" "}
+              &nbsp; Upload Image{" "}
+              <i
+                className="las la-cloud-upload-alt"
+                style={{ fontSize: "18px" }}
+              />
+            </Button>{" "}
+          </Col>
+        )}
+      </Row>
     </>
   );
 };
