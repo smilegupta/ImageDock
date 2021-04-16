ab6ea4a59603086ec441f9a088c67463b61d8263
smilegupta <guptamiley3012@gmail.com>
2021-04-16 22:59:15 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './bootstrap.min.css'
+import "react-toastify/dist/ReactToastify.css";
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify'
+import config from "./config/config.json"
+
+
+Amplify.configure({
+  Auth:{
+    mandatorySignId: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId:  config.cognito.APP_CLIENT_ID 
+  }
+});
+
+
 
 ReactDOM.render(
     <App />,
