fa711c9308bcade89b5e10690355e1da02f411fe
smilegupta <guptamiley3012@gmail.com>
2021-04-18 22:57:02 +0530

    validation for login and register page

--- src/Components/Screens/Auth/Login.js
+++ src/Components/Screens/Auth/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
@@ -7,18 +8,85 @@ import FormContainer from "./FormContainer";
 import LoginImage from "../../../Images/signIn.svg";
 import { Auth } from "aws-amplify";
 import { toast } from "react-toastify";
-import PasswordMask from 'react-password-mask';
+import PasswordMask from "react-password-mask";
+import _ from "lodash";
+
 toast.configure();
 
-const Login = ({auth}) => {
+const Login = ({ auth }) => {
+  // State Variables
   let history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [resendEmail, setResendEmail] = useState(false)
+  const [resendEmail, setResendEmail] = useState(false);
+  let [visited, setVisited] = useState({
+    email: false,
+    password: false,
+  });
+  let [errors, setErrors] = useState({});
 
+  // Final Validation
+  const finalValidate = () => {
+    errors = {};
+
+    if (email === undefined || email === "")
+      errors.email = "Please enter your email";
+    else if (
+      !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)
+    )
+      errors.email = "Please enter a valid email address";
+
+    if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password))
+      errors.password = "Please enter a valid password";
+
+    setErrors(errors);
+
+    if (Object.entries(errors).length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Initial Validation
+  const doValidate = () => {
+    errors = {};
+
+    if ((email === undefined || email === "") && visited.email)
+      errors.email = "Please enter your email";
+    else if (
+      !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)
+    )
+      errors.email = "Please enter a valid email address";
+
+    if (
+      !/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password) &&
+      visited.password
+    )
+      errors.password = "Please enter a valid password";
+
+    setErrors(errors);
+
+    if (Object.entries(errors).length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Handle Visited Fields
+  const handleVisited = (e) => {
+    let { name } = e.target || e;
+    _.set(visited, name, true);
+    setVisited({ ...visited });
+    doValidate();
+  };
+
+  // Login Function
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!finalValidate()) return;
     setLoading(true);
     try {
       const res = await Auth.signIn(email, password);
@@ -32,13 +100,14 @@ const Login = ({auth}) => {
         draggable: true,
       });
       setLoading(false);
-      auth.setAuthenticated(true)
-      auth.setUser(res)
+      auth.setAuthenticated(true);
+      auth.setUser(res);
       history.push(`/home`);
     } catch (err) {
-      let error = err.message
-      if (err.message === 'User is not confirmed.') {
-        error = 'Your account verification not complete. Please complete the verification before logging in.'
+      let error = err.message;
+      if (err.message === "User is not confirmed.") {
+        error =
+          "Your account verification not complete. Please complete the verification before logging in.";
       }
       toast.error(error, {
         position: "top-right",
@@ -48,36 +117,40 @@ const Login = ({auth}) => {
         pauseOnHover: true,
         draggable: true,
       });
-      if (err.message === 'User is not confirmed.') setResendEmail(true);
+      if (err.message === "User is not confirmed.") setResendEmail(true);
       setLoading(false);
     }
   };
 
+  // Resend Confirmation Link
   const resendConfirmationLink = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       await Auth.resendSignUp(email);
-      toast.success('Verification email resent successfully. Please verify your account by clicking that link before logging in.', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true
-      });
-      setResendEmail(false)
+      toast.success(
+        "Verification email resent successfully. Please verify your account by clicking that link before logging in.",
+        {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        }
+      );
+      setResendEmail(false);
     } catch (err) {
-      let error = err.message || 'Something went wrong!';
+      let error = err.message || "Something went wrong!";
       toast.error(error, {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
-        draggable: true
+        draggable: true,
       });
     }
-  }
+  };
 
   return (
     <FormContainer>
@@ -101,10 +174,18 @@ const Login = ({auth}) => {
             onChange={(e) => {
               setEmail(e.target.value);
             }}
+            onBlur={handleVisited}
           />
-          {
-            resendEmail && <p className="text-danger text-right cursor-pointer" onClick={(e) => resendConfirmationLink(e)}> Missed Confirmation Link?</p>
-          }
+          <span className="form-text text-danger">{errors.email || ""}</span>
+          {resendEmail && (
+            <p
+              className="text-danger text-right cursor-pointer"
+              onClick={(e) => resendConfirmationLink(e)}
+            >
+              {" "}
+              Missed Confirmation Link?
+            </p>
+          )}
         </div>
         <div className="form-group">
           <label htmlFor="password">Password*</label>
@@ -117,17 +198,30 @@ const Login = ({auth}) => {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            showButtonContent={(<i className="la la-eye" style={{ background: "transparent !important" }}></i>)}
-            hideButtonContent={(<i className="la la-eye-slash"></i>)}
+            onBlur={handleVisited}
+            showButtonContent={
+              <i
+                className="la la-eye"
+                style={{ background: "transparent !important" }}
+              ></i>
+            }
+            hideButtonContent={<i className="la la-eye-slash"></i>}
           />
-          <p className="text-muted text-right cursor-pointer"> Forgot Password? <Link to = {`/forgot-password/${email}`}><span className="text-dark">
-            Click here</span></Link> </p>
+          <span className="form-text text-danger">{errors.password || ""}</span>
+          <p className="text-muted text-right cursor-pointer">
+            {" "}
+            Forgot Password?{" "}
+            <Link to={`/forgot-password/${email}`}>
+              <span className="text-dark">Click here</span>
+            </Link>{" "}
+          </p>
         </div>
       </form>
       <button
         type="button"
         onClick={(e) => handleSubmit(e)}
         className="btn btn-primary"
+        disabled={loading}
       >
         Sign In {loading ? " " : ""}
         <span
